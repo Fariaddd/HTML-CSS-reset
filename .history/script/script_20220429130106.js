b773ffef571e70e3a7407530b291d2c67ade9457
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-04-30 14:41:51 +0200

    w

--- .history/script/script_20220429130106.js
+++ .history/script/script_20220429130106.js
@@ -3,8 +3,10 @@
 const baseUrl = 'https://successful-wing.flywheelsites.com/wp-json/wc/store/products'
 const productContainer = document.querySelector(".products")
 
-async function getProducts(baseUrl) {
-
+async function getProducts(url) {
+    const response = await fetch(url);
+    const products = await response.json();
+    console.log(products);
 
 
 
--- .history/script/script_20220429130314.js
+++ .history/script/script_20220429130314.js
@@ -4,7 +4,9 @@ const baseUrl = 'https://successful-wing.flywheelsites.com/wp-json/wc/store/prod
 const productContainer = document.querySelector(".products")
 
 async function getProducts(baseUrl) {
-
+    const response = await fetch(baseUrl);
+    const products = await response.json();
+    console.log(products);
 
 
 
